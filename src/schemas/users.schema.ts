be467941b0bddf454175eb8c54a7dbe7745fcc33
krystian john dumapit <23546777+Kryuzaki18@users.noreply.github.com>
2026-05-24 03:27:57 +0800

    chore: update validation char lenth

--- src/schemas/users.schema.ts
+++ src/schemas/users.schema.ts
@@ -22,8 +22,8 @@ const UserSchema: Schema = new Schema(
       type: String,
       required: [true, "Name is required"],
       trim: true,
-      minlength: [2, "Name must be at least 2 characters"],
-      maxlength: [100, "Name must be at most 100 characters"],
+      minlength: [4, "Name must be at least 4 characters"],
+      maxlength: [50, "Name must be at most 50 characters"],
     },
     email: {
       type: String,
